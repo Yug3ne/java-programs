9689a34a2f39469e5653a95b2f274cbf6a85b84e
yugene <eugenekoech9@gmail.com>
2024-09-19 13:06:57 +0200

    Imlemened file handinling to save to do tasks in a file and also be able to load them, Understood the basic concepts of functions

--- TodoList/src/TodoList.java
+++ TodoList/src/TodoList.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.io.*;
 
 public class TodoList {
 
     // create a list to store task
     private static List<String> tasks = new ArrayList<>();
+    private static final String File_name = "todos.txt";
 
     // list of the menu items
     public static void showMenu() {
@@ -13,15 +15,15 @@ public class TodoList {
         System.out.println("1. Add Task");
         System.out.println("2. Remove Task");
         System.out.println("3. View All Tasks");
-        System.out.println("4. Mark Task as Completed");
-        System.out.println("5. Save and Exit");
+        System.out.println("4. Save tasks to file");
+        System.out.println("5. Close the program");
         System.out.print("Choose an option: ");
     }
 
     // method to add something to tasks
     public static void addTask(Scanner scanner) {
         System.out.print("Enter Task: ");
-        String task = scanner.next();
+        String task = scanner.nextLine();
         tasks.add(task);
         System.out.println("Task added to the list");
     }
@@ -45,6 +47,7 @@ public class TodoList {
 
     }
 
+    // view your todo tasks
     public static void viewTask() {
         if (tasks.isEmpty()) {
             System.out.println("Task list is empty");
@@ -54,17 +57,56 @@ public class TodoList {
                 System.out.println(tasks.get(i));
             }
         }
+
+    }
+
+    // save task to file
+    public static void saveTasks() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(File_name))) {
+            for (String task : tasks) {
+                writer.write(task);
+                writer.newLine();
+            }
+            System.out.println("Tasks saved succesfully");
+
+        } catch (IOException e) {
+            System.out.println("Error saving tasks");
+        }
+
+    }
+
+    // load tasks saved on the file
+    public static void loadTasks() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(File_name))) {
+            String task;
+            while ((task = reader.readLine()) != null) {
+                tasks.add(task);
+            }
+            System.out.println("Tasks loaded successfully");
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+
+        catch (IOException e) {
+            System.out.println("Failed to retrieve tasks");
+        }
+    }
+
+    // close the program
+    public static void closeProgram() {
+        System.out.println("Exiting the program....");
+        System.exit(0);
     }
 
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        // loadTasks();
+        loadTasks();
         boolean running = true;
         while (running) {
             showMenu();
             int choice = scanner.nextInt();
             scanner.nextLine();
-
             switch (choice) {
                 case 1:
                     addTask(scanner);
@@ -75,9 +117,16 @@ public class TodoList {
                 case 3:
                     viewTask();
                     break;
+                case 4:
+                    saveTasks();
+                case 5:
+                    running = false;
+                    closeProgram();
                 default:
                     break;
             }
         }
     }
 }
+
+// Todo: => Add a make completed function for the completed tasks
